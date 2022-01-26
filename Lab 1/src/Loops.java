cef1b65315e5debff110e35da902af7c2e1f079d
JosiahSpringer <JosiahSpringer@SMR-5DPLL63>
2022-01-25 23:21:06 -0500

    first commit
    uploading file

---
+++ Lab 1/src/Loops.java
@@ -0,0 +1,14 @@
+public class Loops {
+
+
+    public static void main(String[] args){
+
+        for (int i = 1; i<=5; i+=1){
+            System.out.println( i + " Squared = " + (i*i));
+        }
+
+
+
+    }
+}
+
